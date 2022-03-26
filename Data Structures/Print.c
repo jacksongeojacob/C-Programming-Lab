8d7fc8a1974a31c2f63fd4073b1652acbde36098
jacksongeojacob <94461372+jacksongeojacob@users.noreply.github.com>
2022-03-26 19:50:36 +0530

    Create Print.c

---
+++ Data Structures/Print.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void test();
+void main()
+{
+  clrscr();
+  test();
+  getch();
+}
+void test()
+{
+   int a[25],n,i=0;
+   printf("ENTER THE NUMBER OF ELEMENTS:");
+   scanf("%d",&n);
+   printf("ENTER THE ELEMENTS:");
+   while(i<=n-1)
+      {
+        scanf("%d",&a[i]);
+         i++;
+       }
+     printf("THE RESULTANT ARRAY IS:");
+{
+   for(i=0;i<n;i++)
+    {
+       printf("%d\t",a[i]);
+}
+      }
+   }
